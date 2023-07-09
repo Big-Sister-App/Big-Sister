48b2d9a37ef39c111044b33337ad6ebf6eee127b
mandy-cyber <mandy@hackbeanpot.com>
2023-07-09 16:31:36 -0400

    fixing lint errors

--- website/static/javascript/map.js
+++ website/static/javascript/map.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 let map;
 
 /**
@@ -5,17 +6,17 @@ let map;
  * @return {void}
  */
 async function initMap() {
-  const boston = { lat: 42.360, lng: -71.059 };
-  const { Map } = await google.maps.importLibrary("maps");
+  const boston = {lat: 42.360, lng: -71.059};
+  const {Map} = await google.maps.importLibrary('maps');
 
   // Centering Map around Boston, MA
   map = new Map(
-    document.getElementById('googleMap'),
-    {
-      zoom: 10,
-      center: boston,
-      mapId: 'DEMO_MAP_ID', // figure out what this should be
-    }
+      document.getElementById('googleMap'),
+      {
+        zoom: 10,
+        center: boston,
+        mapId: 'DEMO_MAP_ID', // figure out what this should be
+      },
   );
 
   const reports = await getJson();
@@ -24,23 +25,21 @@ async function initMap() {
   }
 }
 
-
 /**
  * Gathers a list of reports from the JSON file of reports
  * @return {any[]} reports - the list of reports
  */
 function getJson() {
-    return fetch('../databases/geocoded_data.json')
-        .then(response => response.text())
-        .then(jsonString => {
-            const reports = JSON.parse(jsonString);
-            return reports;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+  return fetch('../databases/geocoded_data.json')
+      .then((response) => response.text())
+      .then((jsonString) => {
+        const reports = JSON.parse(jsonString);
+        return reports;
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
 }
-
 
 /**
  * Creates a marker from the given report and adds it to the given map. A
@@ -51,24 +50,23 @@ function getJson() {
  * @return {void}
  */
 async function createMarker(report, map) {
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const {InfoWindow} = await google.maps.importLibrary("maps")
-    const locs = report['gcAddress'].split(',')
-    const position = { lat: parseFloat(locs[0]), lng: parseFloat(locs[1]) }
-    // marker
-    const marker = new AdvancedMarkerElement({
-        map: map,
-        position: position,
-        title: reportToString(report)
-    })
-    // info window
-    var infoWindow = new google.maps.InfoWindow({});
-    marker.addListener('click', function() {
-      infoWindow.setContent(marker.title);
-      infoWindow.open(map, marker);
-    });
+  const {AdvancedMarkerElement} = await google.maps.importLibrary('marker');
+  const {InfoWindow} = await google.maps.importLibrary('maps');
+  const locs = report['gcAddress'].split(',');
+  const position = {lat: parseFloat(locs[0]), lng: parseFloat(locs[1])};
+  // marker
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: reportToString(report),
+  });
+  // info window
+  const infoWindow = new google.maps.InfoWindow({});
+  marker.addListener('click', function() {
+    infoWindow.setContent(marker.title);
+    infoWindow.open(map, marker);
+  });
 }
-
 
 /**
  * Formats a report as a multi-line string to be displayed to an end-user
@@ -89,9 +87,7 @@ function reportToString(report) {
     ${reportLoc}
     <hr>
     ${reportDesc}
-`;
+  `;
 
-
-  return reportString
+  return reportString;
 }
-
